ac7a74c4e92a2cd595c2b8a53f936d887a579f00
cmakrionitis <cmak@unipi.gr>
2026-06-08 03:21:42 +0300

    Change Url root

--- elearning/elearning/urls.py
+++ elearning/elearning/urls.py
@@ -20,6 +20,8 @@ from django.contrib.auth import views as auth_views
 from account import views as views_account
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,7 +31,9 @@ urlpatterns = [
     path('accounts/logout/', views_account.author_logout, name='logout'),
     path('elearning/', include('courses.urls')),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
 ]
 
-urlpatterns += [
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
