5f8e75bb7e65141493ebabbced6ea6b95943bef4
cmakrionitis <cmak@unipi.gr>
2026-06-08 14:57:00 +0300

    Change Settings 08-06

--- elearning/elearning/settings.py
+++ elearning/elearning/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+# Settings Change
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
